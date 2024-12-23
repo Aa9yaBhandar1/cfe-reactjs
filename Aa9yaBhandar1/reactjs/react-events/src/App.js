3a8490a1ac5f2858105cbfd4d4667c121a01402f
Aa9yaBhandar1 <aagyabhandari222@gmail.com>
2024-12-23 21:46:39 +0545

    created todo folder

---
+++ Aa9yaBhandar1/reactjs/react-events/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+    <Todo/>
+        <h1>Hello World </h1>
+        <main>
+          <Todo Title={"Learn React"} Description={"A todo application is here"}/>
+                  </main>
+    </>);
+}
+
+export default App;
---
+++ Aa9yaBhandar1/reactjs/react-events/src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todo({Title, Description}) {
+  return (
+    <><nav>Todo App</nav>
+    <h2>{Title}</h2>
+    <div>{Description}</div>
+    </>
+  );
+}
+
+export default Todo
